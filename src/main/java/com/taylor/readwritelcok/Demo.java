7c65d006cfe6392b2ec0f657d09eee8ca7e816aa
xiaolu.zhang <516195940@qq.com>
2017-10-25 16:47:45 +0800

    更新

--- src/main/java/com/taylor/readwritelcok/Demo.java
+++ src/main/java/com/taylor/readwritelcok/Demo.java
@@ -11,10 +11,10 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class Demo {
     public static void main(String[] args) {
         ObjectO o = new ObjectO();
-        ThreadT t1 = new ThreadT(o, "t1");
-        ThreadT t2 = new ThreadT(o, "t2");
-        ThreadT t3 = new ThreadT(o, "t3");
-        ThreadT t4 = new ThreadT(o, "t4");
+        Thread t1 = new ThreadRead(o, "t1");
+        Thread t2 = new ThreadRead(o, "t2");
+        Thread t3 = new ThreadWrite(o, "t3");
+        Thread t4 = new ThreadWrite(o, "t4");
         t1.start();
         t2.start();
         t3.start();
@@ -22,10 +22,10 @@ public class Demo {
     }
 }
 
-class ThreadT extends Thread {
+class ThreadRead extends Thread {
     private ObjectO object;
 
-    ThreadT(ObjectO object, String name) {
+    ThreadRead(ObjectO object, String name) {
         super(name);
         this.object = object;
     }
@@ -33,20 +33,38 @@ class ThreadT extends Thread {
 
     @Override
     public void run() {
-        object.read();
-        object.write();
+        while (true) {
+            object.read();
+        }
+    }
+}
+
+class ThreadWrite extends Thread {
+    private ObjectO object;
+
+    ThreadWrite(ObjectO object, String name) {
+        super(name);
+        this.object = object;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            object.write();
+        }
     }
 }
 
 class ObjectO {
-    private Lock readLock = new ReentrantReadWriteLock().readLock();
-    private Lock writeLock = new ReentrantReadWriteLock().writeLock();
+    ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private Lock readLock = lock.readLock();
+    private Lock writeLock = lock.writeLock();
 
     public void read() {
         try {
             readLock.lock();
             System.out.println(Thread.currentThread().getName() + ":I am coming in--reading");
-            Thread.sleep(5000);
+            Thread.sleep(100);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -58,7 +76,7 @@ class ObjectO {
         try {
             writeLock.lock();
             System.out.println(Thread.currentThread().getName() + ":I am coming in--writing");
-            Thread.sleep(5000);
+            Thread.sleep(100);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
--- src/main/java/com/taylor/weather/Weather.java
+++ src/main/java/com/taylor/weather/Weather.java
@@ -3,6 +3,8 @@ package com.taylor.weather;
 import lombok.Data;
 
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author xiaolu.zhang
@@ -14,28 +16,45 @@ public class Weather {
     public int tempreture;
     public int wet;
     public int wind;
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private Lock readLock = lock.readLock();
+    private Lock writeLock = lock.writeLock();
 
-    public synchronized void write() {
+
+    public void write() {
         try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
+            writeLock.lock();
+            Random random = new Random();
+            this.tempreture = random.nextInt(100);
+            this.wet = random.nextInt(100);
+            this.wind = random.nextInt(100);
+            System.out.println("写：当前温度：" + this.getTempreture());
+            System.out.println("写：当前湿度：" + this.getWet());
+            System.out.println("写：当前风速：" + this.getWind());
+            System.out.println("************************************");
+            System.out.println();
+            Thread.sleep(100);
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            writeLock.unlock();
         }
-        Random random = new Random();
-        this.tempreture = random.nextInt(100);
-        this.wet = random.nextInt(100);
-        this.wind = random.nextInt(100);
+
     }
 
-
-    public synchronized void read() {
+    public void read() {
         try {
-            Thread.sleep(1);
-        } catch (InterruptedException e) {
+            readLock.lock();
+            System.out.println("读：当前温度：" + this.getTempreture());
+            System.out.println("读：当前湿度：" + this.getWet());
+            System.out.println("读：当前风速：" + this.getWind());
+            System.out.println("************************************");
+            System.out.println();
+            Thread.sleep(1000);
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            readLock.unlock();
         }
-        System.out.println("当前温度：" + this.getTempreture());
-        System.out.println("当前湿度：" + this.getWet());
-        System.out.println("当前风速：" + this.getWind());
     }
 }
