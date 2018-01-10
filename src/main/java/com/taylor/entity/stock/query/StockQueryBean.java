f14b8403f24e7a15b2282251b117a6ad8c54f264
xiaolu.zhang <516195940@qq.com>
2018-01-10 18:36:46 +0800

    更新

---
+++ src/main/java/com/taylor/entity/stock/query/StockQueryBean.java
@@ -0,0 +1,25 @@
+package com.taylor.entity.stock;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author xiaolu.zhang
+ * @desc:
+ * @date: 2018/1/5 16:26
+ */
+@Data
+public class StockQueryBean extends BaseQueryBean implements Serializable {
+
+    private  static final long serialVersionUID = 3930601888186903572L;
+
+    private  String step;
+
+    private String start;
+
+    private String count;
+
+    private String fq_type;
+
+}
---
+++ src/main/java/com/taylor/entity/stock/query/TimeStockDataQueryBean.java
@@ -0,0 +1,9 @@
+package com.taylor.entity.stock.query;
+
+/**
+ * @author xiaolu.zhang
+ * @desc:
+ * @date: 2018/1/10 17:15
+ */
+public class TimeStockDataQueryBean {
+}
