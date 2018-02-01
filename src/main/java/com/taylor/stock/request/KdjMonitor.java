48a619da687e6eb1826061d956c5b8b05dad2e7f
xiaolu.zhang <516195940@qq.com>
2018-02-01 15:39:15 +0800

    更新

--- src/main/java/com/taylor/stock/request/KdjMonitor.java
+++ src/main/java/com/taylor/stock/request/KdjMonitor.java
@@ -4,6 +4,10 @@ import com.taylor.common.CommonRequest;
 import com.taylor.entity.stock.StockPanKouData;
 import lombok.Data;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static com.taylor.common.MailUtils.sendMail;
 import static com.taylor.common.SoundUtil.paly;
 
@@ -15,23 +19,28 @@ import static com.taylor.common.SoundUtil.paly;
 @Data
 public class KdjMonitor extends Thread {
 
-    public static volatile int a=0;
+    public static volatile int a = 0;
 
     private String stockCode;
 
-    public  KdjMonitor(String stockCode){
-        this.stockCode=stockCode;
+    public KdjMonitor(String stockCode) {
+        this.stockCode = stockCode;
     }
 
     @Override
     public void run() {
-        while (a==0) {
+        while (a == 0) {
+            /**如果超过14：40则逢高抛出所有股票**/
+            if(DateCompare(new SimpleDateFormat("HH:MM").format(new Date()), "14:40", "HH:mm")>=0){
+                paly("audio/alarm.wav");
+                sendMail("时间警告","当前时间超过14：40，后期跳水，逢高全抛，见好就收" );
+            }
             int check = KdjTimeDataRequest.check(stockCode);
             StockPanKouData stockFundInOutData = CommonRequest.getStockPanKouData(stockCode);
-            if(check==1){
+            if (check == 1) {
                 paly("audio/goodNew.wav");
-                if(stockFundInOutData!=null) {
-                    sendMail(stockFundInOutData.getStockName()+"-->立即买进","股票("+stockFundInOutData.getStockName()+")出现临界值，请立即买进手上的股票");
+                if (stockFundInOutData != null) {
+                    sendMail(stockFundInOutData.getStockName() + "-->立即买进", "股票(" + stockFundInOutData.getStockName() + ")出现临界值，请立即买进手上的股票");
                     try {
                         Thread.sleep(5000);
                     } catch (InterruptedException e) {
@@ -39,10 +48,10 @@ public class KdjMonitor extends Thread {
                     }
                 }
             }
-            if(check==-1){
+            if (check == -1) {
                 paly("audio/alarm.wav");
-                if(stockFundInOutData!=null) {
-                    sendMail(stockFundInOutData.getStockName()+"-->立即抛售","股票("+stockFundInOutData.getStockName()+")出现临界值，请立即抛售手上的股票");
+                if (stockFundInOutData != null) {
+                    sendMail(stockFundInOutData.getStockName() + "-->立即抛售", "股票(" + stockFundInOutData.getStockName() + ")出现临界值，请立即抛售手上的股票");
                     try {
                         Thread.sleep(5000);
                     } catch (InterruptedException e) {
@@ -50,10 +59,10 @@ public class KdjMonitor extends Thread {
                     }
                 }
             }
-            if(check==2){
+            if (check == 2) {
                 paly("audio/timeCount.wav");
-                if(stockFundInOutData!=null) {
-                    sendMail(stockFundInOutData.getStockName()+"-->预警","股票("+stockFundInOutData.getStockName()+")出现临界值，请留意");
+                if (stockFundInOutData != null) {
+                    sendMail(stockFundInOutData.getStockName() + "-->预警", "股票(" + stockFundInOutData.getStockName() + ")出现临界值，请留意");
                     try {
                         Thread.sleep(5000);
                     } catch (InterruptedException e) {
@@ -61,8 +70,8 @@ public class KdjMonitor extends Thread {
                     }
                 }
             }
-            if(check==0){
-                System.out.println(stockFundInOutData.getStockName()+"一切正常，正在密切监视");
+            if (check == 0) {
+                System.out.println(stockFundInOutData.getStockName() + "一切正常，正在密切监视");
             }
             try {
                 Thread.sleep(60000);
@@ -72,10 +81,23 @@ public class KdjMonitor extends Thread {
         }
     }
 
+    public static int DateCompare(String source, String traget, String type) {
+        int ret = 2;
+        try {
+            SimpleDateFormat format = new SimpleDateFormat(type);
+            Date sourcedate = format.parse(source);
+            System.out.println(sourcedate);
+            Date tragetdate = format.parse(traget);
+            System.out.println(tragetdate);
+            ret = sourcedate.compareTo(tragetdate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
     public static void main(String... args) {
         KdjMonitor kdjMonitor1 = new KdjMonitor("sh510900");
-        KdjMonitor kdjMonitor2 = new KdjMonitor("sh510710");
         kdjMonitor1.start();
-        kdjMonitor2.start();
     }
 }
--- src/main/java/com/taylor/stock/request/KdjTimeDataRequest.java
+++ src/main/java/com/taylor/stock/request/KdjTimeDataRequest.java
@@ -13,6 +13,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
