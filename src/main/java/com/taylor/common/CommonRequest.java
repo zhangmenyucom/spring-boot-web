72e83db914fe1af56f2029d4c0f41d64b058161a
xiaolu.zhang <516195940@qq.com>
2018-04-08 11:41:39 +0800

    merge

--- src/main/java/com/taylor/common/CommonRequest.java
+++ src/main/java/com/taylor/common/CommonRequest.java
@@ -253,6 +253,6 @@ public class CommonRequest<T> {
     }
 
     public static void main(String... args) {
-        System.out.println(JsonUtil.transfer2JsonString(getStckTodayBaseInfo("SH600191")));
+        System.out.println(JsonUtil.transfer2JsonString(getStckDailyHistory("SH600191",2)));
     }
 }
--- src/main/java/com/taylor/stock/strategy/FiveOverTenStrategy.java
+++ src/main/java/com/taylor/stock/strategy/FiveOverTenStrategy.java
@@ -1,6 +1,9 @@
 package com.taylor.stock.strategy;
 
+import com.taylor.common.CommonRequest;
 import com.taylor.entity.stock.MashData;
+import com.taylor.entity.stock.StockPanKouData;
+import com.taylor.entity.stock.TencentDayData;
 import com.taylor.stock.common.StrategyEnum;
 
 import java.util.List;
@@ -19,9 +22,24 @@ public class FiveOverTenStrategy extends IStrategy {
     @Override
     public int doCheck(List<MashData> mashDataList) {
         MashData mashDataToday = mashDataList.get(0);
-        if(mashDataToday.getKline().getNetChangeRatio()<0){
+        StockPanKouData stockPanKouData = CommonRequest.getStockPanKouData(mashDataToday.getStockCode().toLowerCase());
+
+        /**今日上涨**/
+        if (mashDataToday.getKline().getNetChangeRatio() <= 0) {
             return 0;
         }
+        /**换手率大于1**/
+        if (stockPanKouData.getExchangeRatio() < 1) {
+            return 0;
+        }
+        List<TencentDayData> stckDailyHistory = CommonRequest.getStckDailyHistory(mashDataList.get(0).getStockCode(), 2);
+
+        /**放量**/
+        if (stckDailyHistory.get(1).getTotalHands() - stckDailyHistory.get(0).getTotalHands() < 0) {
+            return 0;
+        }
+
+        /**5日线突破10日**/
         MashData mashDataYestoday = mashDataList.get(1);
         if ((mashDataToday.getMa5().getAvgPrice() - mashDataToday.getMa10().getAvgPrice() > 0) && (mashDataYestoday.getMa5().getAvgPrice() - mashDataYestoday.getMa10().getAvgPrice() < 0)) {
             return 1;
