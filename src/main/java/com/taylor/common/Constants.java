7d51b9516dc80eae6ef32d724863e164713fd24a
zhangmenyucom <zhangmenyucom@github.com>
2018-08-10 01:35:01 +0800

    更新

--- src/main/java/com/taylor/common/Constants.java
+++ src/main/java/com/taylor/common/Constants.java
@@ -7,7 +7,7 @@ package com.taylor.common;
  */
 public class Constants {
    public static final String COOKIE="ASP.NET_SessionId=4v0k3dovol5uopxdzlhaoslz; ValidateToken=371666baebecfe3530ae22257d70a255; skinStyle=suolaier; CurrentSkin=suolaier; SESSION_COOKIE=1; XYHandicap=0; multiSelect=false; GeneralizToken=ec19f0526a45451c991831395f0ca35b; LoginSessionID=854d620e372453a0f2d955604a1c5491; GAMEID=123";
-   public static final int initTime=1;
+   public static final int initTime=2;
 }
 
 
