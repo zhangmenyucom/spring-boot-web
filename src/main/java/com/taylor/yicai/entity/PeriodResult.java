aed5b3ff89dae51ddbf3cda4b1381ed407c7f374
zhangmenyucom <516195940@qq.com>
2018-08-09 09:16:00 +0800

    更新

---
+++ src/main/java/com/taylor/yicai/entity/PeriodResult.java
@@ -0,0 +1,19 @@
+package com.taylor.yicai.entity;/**
+ * ${author} on 2018/8/9.
+ */
+
+import lombok.Data;
+
+/**
+ * Add some description about this class.
+ *
+ * @author zhangxiaolu
+ * @since 2018/8/9 9:14
+ */
+@Data
+public class PeriodResult {
+    private String id;
+    private String date;
+    private String result;
+    private String year;
+}
