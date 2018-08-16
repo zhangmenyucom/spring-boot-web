dc2aff96ff0fb4b501d3527753b012762bb7e776
zhangmenyucom <516195940@qq.com>
2018-08-16 18:59:48 +0800

    更新

---
+++ src/main/java/com/taylor/java8/OptionTest.java
@@ -0,0 +1,16 @@
+package com.taylor.java8;
+
+import java.util.Optional;
+
+/**
+/**
+ * @author zhangxiaolu
+ * @描述
+ * @since 2018/8/16 17:34
+ */
+public class OptionTest {
+    public static void main(String[] args) {
+        System.out.println(Optional.empty());
+
+    }
+}
