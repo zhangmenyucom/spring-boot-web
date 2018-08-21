5e4d8e833fe038d9c25b7cf256b87a70a9b57df0
zhangmenyucom <516195940@qq.com>
2018-08-21 11:18:55 +0800

    更新

---
+++ src/main/java/com/taylor/java8/ListTest.java
@@ -0,0 +1,25 @@
+package com.taylor.java8;/**
+ * ${author} on 2018/8/21.
+ */
+
+
+
+import lombok.val;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author zhangxiaolu
+ * @描述
+ * @since 2018/8/21 11:04
+ */
+
+public class ListTest {
+    public static void main(String[] args) {
+        Integer s=1;
+        val list= Arrays.asList(s,2,2,2);
+        long count = list.stream().filter(i->i<2).count();
+        System.out.println(count);
+    }
+}
