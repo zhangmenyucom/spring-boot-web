6585100ad73ec2732abb82803c81e54dce46c700
zhangmenyucom <516195940@qq.com>
2018-08-29 08:57:36 +0800

    更新

--- src/main/java/com/taylor/api/ApiClient.java
+++ src/main/java/com/taylor/api/ApiClient.java
@@ -2,10 +2,22 @@ package com.taylor.api;/**
  * ${author} on 2018/8/29.
  */
 
+import retrofit2.Retrofit;
+
+import static com.taylor.common.Constants.TENCENT_PREFIX;
+
+
 /**
  * @author zhangxiaolu
  * @描述
  * @since 2018/8/29 8:52
  */
 public class ApiClient {
+    public static final Retrofit retrofit = new Retrofit.Builder().addConverterFactory(new Retrofit2ConverterFactory()).baseUrl(TENCENT_PREFIX).build();
+    public static Api api = retrofit.create(Api.class);
+
+
+    public static void main(String... args) {
+
+    }
 }
