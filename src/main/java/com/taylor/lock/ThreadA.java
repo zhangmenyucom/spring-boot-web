068581f11dd044f11530c7d89df950a5047356ec
zhangmenyucom <516195940@qq.com>
2018-10-31 15:42:38 +0800

    更新

---
+++ src/main/java/com/taylor/lock/ThreadA.java
@@ -0,0 +1,24 @@
+package com.taylor.lock;
+
+public class ThreadA extends Thread {
+    private Service service;
+
+    public ThreadA(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.seckill();
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Service service = new Service();
+        for (int i = 0; i < 200; i++) {
+            ThreadA threadA = new ThreadA(service);
+            threadA.start();
+        }
+    }
+}
