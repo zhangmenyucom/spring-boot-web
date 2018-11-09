1cb29f1391660e921c41f72166b6d4b5a6b742a0
zhangmenyucom <516195940@qq.com>
2018-11-09 11:09:51 +0800

    更新

--- src/main/java/com/taylor/api/ApiClient.java
+++ src/main/java/com/taylor/api/ApiClient.java
@@ -287,6 +287,24 @@ public class ApiClient {
         StockBaseInfo biaozhi = new StockBaseInfo();
         biaozhi.setStockName("标指");
         biaozhi.setNetChangeRatio(BigDecimal.valueOf(Double.parseDouble(getBigData("usINX").split("~")[32])));
+
+        StockBaseInfo pinan = new StockBaseInfo();
+        pinan.setStockName("中国平安");
+        StockPanKouData sh601318 = getPanKouData("sh601318");
+        pinan.setNetChangeRatio(BigDecimal.valueOf(sh601318.getUpDownMountPercent()));
+
+        StockBaseInfo maotai = new StockBaseInfo();
+        maotai.setStockName("贵州茅台");
+        StockPanKouData sh600519 = getPanKouData("sh600519");
+        maotai.setNetChangeRatio(BigDecimal.valueOf(sh600519.getUpDownMountPercent()));
+
+        StockBaseInfo shiyou = new StockBaseInfo();
+        shiyou.setStockName("中国石油");
+        StockPanKouData sh601857 = getPanKouData("sh601857");
+        shiyou.setNetChangeRatio(BigDecimal.valueOf(sh601857.getUpDownMountPercent()));
+
+
+
         List<StockBaseInfo> stockBigDataList = new ArrayList<>();
         stockBigDataList.add(shangzhi);
         stockBigDataList.add(shenzhi);
@@ -295,6 +313,9 @@ public class ApiClient {
         stockBigDataList.add(nazhi);
         stockBigDataList.add(daozhi);
         stockBigDataList.add(biaozhi);
+        stockBigDataList.add(pinan);
+        stockBigDataList.add(maotai);
+        stockBigDataList.add(shiyou);
         return stockBigDataList;
 
     }
