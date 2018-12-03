51516c39994efab09a7c400e0139f4651ef35387
zhangmenyucom <516195940@qq.com>
2018-12-03 11:48:07 +0800

    更新

--- src/main/java/com/taylor/common/ConstantsInits.java
+++ src/main/java/com/taylor/common/ConstantsInits.java
@@ -9,16 +9,6 @@ import java.util.*;
  */
 public class ConstantsInits {
 
-    public static List<String> STOCK_CODE_LIST_MAIN = new ArrayList<>(1000);
-    public static List<String> STOCK_CODE_LIST_ZHONGXIAO = new ArrayList<>(1000);
-    public static List<String> STOCK_CODE_LIST_CHUANGYE = new ArrayList<>(1000);
-
-    static {
-        STOCK_CODE_LIST_MAIN = Arrays.asList(Constants.STOCK_CODE_MAIN_BAN.split(","));
-        STOCK_CODE_LIST_ZHONGXIAO = Arrays.asList(Constants.STOCK_CODE_ZHONGXIAO_BAN.split(","));
-        STOCK_CODE_LIST_CHUANGYE = Arrays.asList(Constants.STOCK_CODE_CHUANGYE_BAN.split(","));
-    }
-
     /**
      * 监控列表
      **/
@@ -34,5 +24,5 @@ public class ConstantsInits {
     /**
      * 成本监控
      **/
-    public static int COST_MONITOR = 1;
+    public static int COST_MONITOR = 0;
 }
