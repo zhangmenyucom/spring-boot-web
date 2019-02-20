5b45b229cb9a72224c23fe362ea46dc57c234372
zhangmenyucom <zhangmenyucom@github.com>
2019-02-20 23:43:15 +0800

    更新

--- src/main/java/com/taylor/config/ScheduleConfig.java
+++ src/main/java/com/taylor/config/ScheduleConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
-import java.util.Calendar;
-import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -27,13 +25,4 @@ public class ScheduleConfig {
         return new ConcurrentTaskScheduler();
     }
 
-    public static void main(String[] args) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        cal.add(Calendar.DAY_OF_YEAR,1);
-        cal.set(Calendar.HOUR_OF_DAY, 12);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
-        System.out.println(cal.getTime());
-    }
 }
